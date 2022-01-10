b7733d0ef7bd8dfdc1668c98934ee59f9acb17b3
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-10 12:01:36 +0500

    third commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # except every yeat that is evenly divisible by 100
 # unless the year  is also evenly  divisible by 400
 
-
 year = int(input("Which year do you want to check? \n"))
 
 if year % 4 == 0:
